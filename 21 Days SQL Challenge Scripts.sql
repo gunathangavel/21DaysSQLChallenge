8030771ff9e6869a0ff7fe523b4baebcf96e63cf
=gunathangavel <=guna.thangavel36@gmail.com>
2025-11-04 10:05:22 -0500

    Updated Day 2 SQL Challenge script

--- 21 Days SQL Challenge Scripts.sql
+++ 21 Days SQL Challenge Scripts.sql
@@ -2,3 +2,8 @@
 
 -- ** Day 1 Challenge ** List all unique hospital services available in the hospital
 select distinct service from services_weekly;
+
+/* ** Day 2 Challege -  Find all patients admitted to 'Surgery' service with a satisfaction score below 70, 
+showing their patient_id, name, age, and satisfaction score */
+
+select patient_id, name, age,  satisfaction  from patient where service='surgery' and satisfaction < 70;
