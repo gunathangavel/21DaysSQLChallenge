9ee3519593cc0259d75c30f07faab9eb5f51f8b8
=gunathangavel <=guna.thangavel36@gmail.com>
2025-11-05 15:20:11 -0500

    Updated Day 3 SQL Challenge Scripts.sql

--- 21 Days SQL Challenge Scripts.sql
+++ 21 Days SQL Challenge Scripts.sql
@@ -7,3 +7,9 @@ select distinct service from services_weekly;
 showing their patient_id, name, age, and satisfaction score */
 
 select patient_id, name, age,  satisfaction  from patient where service='surgery' and satisfaction < 70;
+
+/*Retrieve the top 5 weeks with the highest patient refusals across all services,
+showing week, service, patients_refused, and patients_request. 
+Sort by patients_refused in descending order*/
+select week, service, patients_refused, 
+patients_request from services_weekly order by patients_refused desc limit 5;
